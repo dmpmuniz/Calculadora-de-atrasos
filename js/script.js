c8b623c02c81ec9a46e20f9c42f6c876b92f9b0a
Daniel Muniz <72808270+dmpmuniz@users.noreply.github.com>
2025-01-30 13:05:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botaoModoEscuro = document.getElementById("modoEscuro");
+    const botaoAltoContraste = document.getElementById("altoContraste");
+    const botaoAumentarFonte = document.getElementById("aumentarFonte");
+    const botaoDiminuirFonte = document.getElementById("diminuirFonte");
+    const body = document.body;
 
+    botaoModoEscuro.addEventListener("click", function() {
+        body.classList.toggle("modo-escuro");
+    });
+
+    botaoAltoContraste.addEventListener("click", function() {
+        body.classList.toggle("alto-contraste");
+    });
+
+    botaoAumentarFonte.addEventListener("click", function() {
+        body.classList.remove("fonte-menor");
+        body.classList.add("fonte-maior");
+    });
+
+    botaoDiminuirFonte.addEventListener("click", function() {
+        body.classList.remove("fonte-maior");
+        body.classList.add("fonte-menor");
+    });
+});
