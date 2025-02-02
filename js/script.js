819d5ca0a1c996f4de04f348a6cc22ac06232918
Daniel Muniz <72808270+dmpmuniz@users.noreply.github.com>
2025-02-02 18:15:56 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("calcForm");
+document.getElementById("atrasoForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const dataRetirada = new Date(document.getElementById("dataRetirada").value);
+  const dataPrevista = new Date(document.getElementById("dataPrevista").value);
+  const dataEntrega = new Date(document.getElementById("dataEntrega").value);
   const resultadoDiv = document.getElementById("resultado");
- 
-   // Cálculo do atraso
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    calcularAtraso();
-  });
 
-  function calcularAtraso() {
-    const dataRetirada = new Date(document.getElementById("dataRetirada").value);
-    const dataPrevista = new Date(document.getElementById("dataPrevista").value);
-    const dataEntrega = new Date(document.getElementById("dataEntrega").value);
+  if (dataEntrega > dataPrevista) {
+    const atrasoEmMilissegundos = dataEntrega - dataPrevista;
+    const atrasoEmDias = Math.ceil(atrasoEmMilissegundos / (1000 * 60 * 60 * 24));
 
-    if (dataEntrega > dataPrevista) {
-      const atrasoEmDias = Math.ceil((dataEntrega - dataPrevista) / (1000 * 60 * 60 * 24));
-      const dataSuspensao = new Date(dataEntrega);
-      dataSuspensao.setDate(dataSuspensao.getDate() + atrasoEmDias);
-      const dataLimite = new Date(dataEntrega);
-      dataLimite.setMonth(dataLimite.getMonth() + 3);
+    const dataSuspensao = new Date(dataEntrega);
+    dataSuspensao.setDate(dataSuspensao.getDate() + atrasoEmDias);
 
-      if (dataSuspensao > dataLimite) {
-        resultadoDiv.innerHTML = `Tempo de atraso: ${atrasoEmDias} dias.<br>Suspenso até: ${dataLimite.toLocaleDateString()} (limite máximo de 3 meses).`;
-      } else {
-        resultadoDiv.innerHTML = `Tempo de atraso: ${atrasoEmDias} dias.<br>Suspenso até: ${dataSuspensao.toLocaleDateString()}.`;
-      }
+    const dataLimite = new Date(dataEntrega);
+    dataLimite.setMonth(dataLimite.getMonth() + 3);
+
+    if (dataSuspensao > dataLimite) {
+      resultadoDiv.innerHTML = `Tempo de atraso: ${atrasoEmDias} dias.<br>Suspenso até: ${dataLimite.toLocaleDateString()} (limite máximo de 3 meses).`;
     } else {
-      resultadoDiv.innerHTML = "Sem atraso. Obrigado por devolver no prazo!";
+      resultadoDiv.innerHTML = `Tempo de atraso: ${atrasoEmDias} dias.<br>Suspenso até: ${dataSuspensao.toLocaleDateString()}.`;
     }
+  } else {
+    resultadoDiv.innerHTML = "Sem atraso. Obrigado por devolver no prazo!";
   }
+
+  // Exibir a área de resultado
+  resultadoDiv.style.display = "block";
 });
